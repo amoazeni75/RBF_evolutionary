c72cfe43bc0dcba638c422bc5eff953090200aac
alireza <amoazeni75@gmail.com>
2019-06-02 13:04:49 +0430

    - initialization parameter done - producing first generation done

--- functions.py
+++ functions.py
@@ -1,22 +1,40 @@
-import random
+from numpy import random
 
 
-def create_initial_generation(inp, generation_size):
-    indexes = random.sample(range(inp.shape[0]), int(generation_size))
+def create_initial_generation(data_set, min_length_chromosome, max_length_chromosome, generation_size, max_sigma):
+    initial_chromosomes = []
+    max_range, min_range = get_max_min_range_dataset(data_set)
+    # create list of chromosome length with the size of generation_size
+    chromosomes_length = random.randint(min_length_chromosome, max_length_chromosome, generation_size)
+    # create list of sigma values with the size of generation_size
+    sigma_values = random.uniform(0.1 * max_sigma, max_sigma, generation_size)
 
-    return indexes
+    for i in range(generation_size):
+        current_chromosome = []
+        a_values = random.uniform((max_range - min_range) * 0.2, (max_range - min_range) * 0.8, chromosomes_length[i])
+        b_values = random.uniform((max_range - min_range) * 0.2, (max_range - min_range) * 0.8, chromosomes_length[i])
+        for j in range(chromosomes_length[i]):
+            for k in range(1, data_set.shape[1]):
+                current_chromosome.append(random.uniform(data_set[k].min(), data_set[k].max()))
+            current_chromosome.append(a_values[j])
+            current_chromosome.append(b_values[j])
+
+        current_chromosome.append(sigma_values[i])
+        initial_chromosomes.append(current_chromosome)
+
+    return initial_chromosomes
 
 
 # we take the minimum length of chromosome equal to number of class in
 # classification mode and 0.2 * class_labels_size in regression mode
 # also we take maximum length of chromosome = 0.4 * data_set_length
 # initial_number_chromosomes = 0.4 * data set size
-
-@TODO min_length_chromosome,max_length_chromosome,\
-      initial_number_chromosomes, max_sigma_mutation
-        give as parameter
-
-def initialization_parameter(data_set, regression_threshold):
+def initialization_parameter(data_set,
+                             regression_threshold,
+                             ratio_min_length_chromosome_reg,
+                             ratio_max_length_chromosome,
+                             ratio_max_sigma,
+                             ratio_initial_number_chromosomes):
     # first finding algorithm mode
     class_labels_size = len(data_set[data_set.shape[1]].unique())
     if class_labels_size <= 2:
@@ -27,20 +45,25 @@ def initialization_parameter(data_set, regression_threshold):
         min_length_chromosome = class_labels_size
     else:
         algorithm_mode = 'Regression'
-        min_length_chromosome = int(0.2 * class_labels_size)
+        min_length_chromosome = int(ratio_min_length_chromosome_reg * class_labels_size)
 
-    max_length_chromosome = int(data_set.shape[0] * 0.4)
+    max_length_chromosome = int(data_set.shape[0] * ratio_max_length_chromosome)
 
+    max_range, min_range = get_max_min_range_dataset(data_set)
+    max_sigma_mutation = (max_range - min_range) * ratio_max_sigma
+    initial_number_chromosomes = int(ratio_initial_number_chromosomes * data_set.shape[0])
+
+    return algorithm_mode, \
+           min_length_chromosome, \
+           max_length_chromosome, \
+           max_sigma_mutation, \
+           initial_number_chromosomes
+
+
+def get_max_min_range_dataset(data_set):
     max_range = -9223372036854775807
     min_range = 9223372036854775807
     for i in range(1, data_set.shape[1]):
         max_range = max(max_range, data_set[i].max())
         min_range = min(min_range, data_set[i].min())
-    max_sigma_mutation = (max_range - min_range) * 0.1
-    initial_number_chromosomes = 0.4 * data_set.shape[0]
-
-    return algorithm_mode, \
-           min_length_chromosome,\
-           max_length_chromosome, \
-           max_sigma_mutation,\
-           initial_number_chromosomes
+    return max_range, min_range
--- run.py
+++ run.py
@@ -6,7 +6,13 @@ import functions
 parser = argparse.ArgumentParser(
     description='This Programme try to train an RBF Network with the help of ES Algorithm')
 
-parser.add_argument('--input', help='Path of input data', default="training_set.xlsx")
+parser.add_argument('--input', help='Path of input data', default="Book1.xlsx")
+parser.add_argument('--cminlcr', help='Minimum Ratio of Chromosome Length in Regression', default="0.2")
+parser.add_argument('--cmaxlcr', help='Maximum Ratio of Chromosome Length', default="0.4")
+parser.add_argument('--init_ch_nu', help='Initial Ratio of Generation', default="0.4")
+parser.add_argument('--cmaxs', help='Maximum Ratio of Sigma', default="0.1")
+parser.add_argument('--reg_thr', help='threshold that indicate running mode', default="0.4")
+
 args = parser.parse_args()
 
 # parameters
@@ -17,20 +23,35 @@ initial_number_chromosomes = -1         # number of initial generation
 dataset_length = 0                      # number of data in dataset
 algorithm_mode = 'Classification_2'
 # indicate running mode of Algorithm {Classification_2, Classification_n, Regression}
-regression_threshold = 0.4              # threshold that indicate running mode
 
+args.cminlcr = float(args.cminlcr)
+args.cmaxlcr = float(args.cmaxlcr)
+args.init_ch_nu = float(args.init_ch_nu)
+args.cmaxs = float(args.cmaxs)
+args.reg_thr = float(args.reg_thr)
 
 # reading data from file with excel format
 dataset_train = pd.read_excel(args.input)
 training_set = dataset_train.iloc[:, 0:dataset_train.shape[1]].values
 dataset_length = dataset_train.shape[0]
 
+
 # initialization of parameters
 algorithm_mode, \
 min_length_chromosome,\
 max_length_chromosome,\
 max_sigma_mutation,\
-initial_number_chromosomes= functions.initialization_parameter(dataset_train, regression_threshold)
+initial_number_chromosomes= functions.initialization_parameter(data_set=dataset_train,
+                                                               regression_threshold=args.reg_thr,
+                                                               ratio_min_length_chromosome_reg=args.cminlcr,
+                                                               ratio_max_length_chromosome=args.cmaxlcr,
+                                                               ratio_max_sigma=args.cmaxs,
+                                                               ratio_initial_number_chromosomes=args.init_ch_nu)
 
 # generate initial generation
-print(algorithm_mode)
+initial_generation = functions.create_initial_generation(data_set=dataset_train,
+                                                         min_length_chromosome=min_length_chromosome,
+                                                         max_length_chromosome=max_length_chromosome,
+                                                         generation_size=initial_number_chromosomes,
+                                                         max_sigma=max_sigma_mutation)
+print(initial_generation)
