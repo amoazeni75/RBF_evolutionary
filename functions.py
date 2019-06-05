43d0b4b97c266b619c2a0600db103abdac2491f2
alireza <amoazeni75@gmail.com>
2019-06-05 17:59:26 +0430

    - algorithm implementation done !!!

--- functions.py
+++ functions.py
@@ -1,24 +1,27 @@
-from numpy import random
 import math
+import numpy as np
+import random as nd
 
 
-def create_initial_generation(data_set, min_length_chromosome, max_length_chromosome, generation_size, max_sigma):
+def create_initial_generation(data_set, min_length_chromosome, max_length_chromosome, generation_size,
+                              max_sigma, data_set_raw):
     initial_chromosomes = []
-    max_range, min_range = get_max_min_range_dataset(data_set)
+
     # create list of chromosome length with the size of generation_size
-    chromosomes_length = random.randint(min_length_chromosome, max_length_chromosome, generation_size)
+    chromosomes_length = np.random.randint(min_length_chromosome, max_length_chromosome, generation_size)
     # create list of sigma values with the size of generation_size
-    makhraj = (data_set.shape[0] * (data_set.shape[1] - 1)) ** (1 / float(data_set.shape[1] - 1))
+    makhraj = (data_set.shape[0] * data_set.shape[1]) ** (1 / float(data_set.shape[1]))
     radial = get_farthest_distance(data_set) / makhraj
-    sigma_values = random.uniform(0.1 * (max_range - min_range), 0.3 * (max_range - min_range), generation_size)
+    sigma_values = np.random.uniform(0.1 * max_sigma, max_sigma, generation_size)
 
     for i in range(generation_size):
         current_chromosome = []
-        radius_values = random.normal(radial, radial * 0.5, chromosomes_length[i])
-        # b_values = random.uniform((max_range - min_range) * 0.2, (max_range - min_range) * 0.8, chromosomes_length[i])
+        radius_values = np.abs(np.random.normal(radial, radial * 0.75, chromosomes_length[i]))
         for j in range(chromosomes_length[i]):
-            for k in range(1, data_set.shape[1]):
-                current_chromosome.append(random.uniform(data_set[k].min(), data_set[k].max()))
+            for k in range(1, data_set_raw.shape[1]):
+                mmin = data_set_raw[k].min()
+                mmax = data_set_raw[k].max()
+                current_chromosome.append(nd.uniform(mmin, mmax))
             current_chromosome.append(radius_values[j])
 
         current_chromosome.append(sigma_values[i])
@@ -74,37 +77,39 @@ def get_max_min_range_dataset(data_set):
 def get_farthest_distance(dataset):
     max_distance = 0
     for i in range(dataset.shape[0]):
-        node1 = dataset.iloc[[i], 0: dataset.shape[1] - 1]
+        node1 = dataset[i]
         for j in range(i + 1, dataset.shape[0]):
-            node2 = dataset.iloc[[j], 0: dataset.shape[1] - 1]
+            node2 = dataset[j]
             max_distance = max(max_distance, get_distance(node1, node2))
     return max_distance
 
 
 def get_distance(p1, p2):
-    p1 = p1.values
-    p2 = p2.values
     sum_num = 0
-    for i in range(p1.shape[1]):
-        sum_num += (p1[0][i] - p2[0][i]) ** 2
+    for i in range(len(p1)):
+        sum_num += (p1[i] - p2[i]) ** 2
     sum_num = math.sqrt(sum_num)
     return sum_num
 
 
 def selecting_parents_random_uniform(seed):
-    return random.choice(seed, 7 * len(seed))
+    parents = []
+    indexes = np.random.randint(0, len(seed), 7 * len(seed))
+    for i in range(len(indexes)):
+        parents.append(seed[indexes[i]])
+    return parents
 
 
 def do_mutation(generation, dimension_size):
     # first we must mutate sigma and then each gene
     for i in range(len(generation)):
         # for j in range(dimension_size, len(generation[i]) - 1, 3):
-        #     generation[i][j] = generation[i][j] * math.exp(-(1 / math.sqrt(dimension_size) * random.normal(0, 1)))
+        #     generation[i][j] = generation[i][j] * math.exp(-(1 / math.sqrt(dimension_size) * np.random.normal(0, 1)))
         #     for k in range(j - dimension_size, j):
-        #         generation[i][k] = generation[i][k] + generation[i][j] * random.normal(0, 1)
-        generation[i][-1] = generation[i][-1] * math.exp(-(1 / math.sqrt(dimension_size) * random.normal(0, 1)))
+        #         generation[i][k] = generation[i][k] + generation[i][j] * np.random.normal(0, 1)
+        generation[i][-1] = generation[i][-1] * math.exp(-((1 / math.sqrt(dimension_size)) * nd.normalvariate(0, 1)))
         for j in range(len(generation[i]) - 1):
-            generation[i][j] = generation[i][j] + generation[i][-1] * random.normal(0, 1)
+            generation[i][j] = generation[i][j] + generation[i][-1] * nd.normalvariate(0, 1)
 
 
 def recombination_chromosomes(generation):
@@ -113,14 +118,13 @@ def recombination_chromosomes(generation):
         chromosome1 = generation[i]
         for j in range(i + 1, len(generation)):
             chromosome2 = generation[j]
-            # if random.uniform(0, 1) <= 0.4:
-            childes.append(do_recombination(chromosome1, chromosome2))
+            if nd.uniform(0, 1) <= 0.4:
+                childes.append(do_recombination(chromosome1, chromosome2))
 
     return childes
 
 
 def do_recombination(ch1, ch2):
-
     if len(ch1) <= len(ch2):
         lower_length = ch1
         higher_length = ch2
@@ -138,5 +142,165 @@ def do_recombination(ch1, ch2):
     return new_child
 
 
-def evaluate_generation(generation, running_mode):
-    return []
+def evaluate_generation(dataset, generation, y_star, running_mode):
+    dimension = dataset.shape[1]
+    # first calculate G matrix
+    list_g_matrices = [[] for i in range(len(generation))]
+    for i in range(dataset.shape[0]):
+        data_i = dataset[i]
+        for j in range(len(generation)):
+            list_g_matrices[j].append(get_gi_of_a_data(data_i, generation[j], dimension))
+
+    # second calculate weights
+    list_w_matrices = []
+    for i in range(len(generation)):
+        list_w_matrices.append(calculate_weight_chromosome_i(list_g_matrices[i], y_star))
+
+    # third calculate Y matrix
+    list_y_matrices = []
+    for i in range(len(generation)):
+        list_y_matrices.append(calculate_y_out(list_g_matrices[i], list_w_matrices[i]))
+
+    # check what is running mode and then calculate corresponding error
+    list_evaluated = []
+
+    if running_mode == "Regression":
+        for i in range(len(generation)):
+            fitness = fitness_regression(list_y_matrices[i], y_star)
+            list_evaluated.append([generation[i], fitness])
+    elif running_mode == "Classification_2":
+        for i in range(len(generation)):
+            fitness = fitness_classification_2(list_y_matrices[i], y_star)
+            # m = (len(generation[i]) - 1) / (dimension + 1)
+            # fitness /= m
+            list_evaluated.append([generation[i], fitness[0]])
+    else:
+        for i in range(len(generation)):
+            fitness = fitness_classification_n(list_y_matrices[i], y_star)
+            list_evaluated.append([generation[i], fitness])
+
+    return list_evaluated
+
+
+def get_gi_of_a_data(data, chromosome, dimension_size):
+    gi = []
+    for index_radius in range(dimension_size, len(chromosome) - 1, (dimension_size + 1)):
+        radius = chromosome[index_radius] ** 2
+        sum_data_dis_center = 0
+        index_data = 0
+        for index_center in range(index_radius - dimension_size, index_radius):
+            sum_data_dis_center += (data[index_data] - chromosome[index_center]) ** 2
+            index_data += 1
+
+        sum_data_dis_center = -sum_data_dis_center / radius
+        sum_data_dis_center = np.exp(sum_data_dis_center)
+        gi.append(sum_data_dis_center)
+
+    return gi
+
+
+def calculate_weight_chromosome_i(gi, y_star):
+    g_new = np.array(gi)
+    g_new_transpose = g_new.transpose()
+    temp = np.dot(g_new_transpose, g_new)
+    temp = temp + 0.001 * np.identity(g_new_transpose.shape[0], dtype=float)
+    temp = np.linalg.inv(temp)
+    temp = np.dot(temp, g_new_transpose)
+    temp = np.dot(temp, y_star)
+    return temp
+
+
+def calculate_y_out(gi, wi):
+    return np.dot(gi, wi)
+
+
+def fitness_regression(y_out, y_star):
+    fitness = 0
+    fitness = np.transpose(y_out - y_star)
+    fitness = np.dot(fitness, y_out - y_star)
+    fitness /= 2
+    fitness = 1 / fitness
+    return fitness
+
+
+def fitness_classification_2(y_out, y_star):
+    fitness = 0
+    for i in range(len(y_out)):
+        # fitness += np.abs(np.sign(y_out[i]) - y_star[i])
+        fitness += np.abs((np.sign(y_out[i]) + 1) / 2 - y_star[i])
+    fitness = fitness / (len(y_star))
+    fitness = 1 - fitness
+    return fitness
+
+
+def fitness_classification_n(y_out, y_star):
+    fitness = 0
+    # ??
+    return fitness
+
+
+def select_based_on_q_tournament(generation, q, selection_size):
+    selected_chromosome = []
+    q = int(q)
+
+    no_need_return_back_selected = False
+    if len(generation) > selection_size:
+        no_need_return_back_selected = True
+
+    for i in range(selection_size):
+        q_selected = nd.sample(range(0, len(generation)), q)
+        fit_best = generation[q_selected[0]][1]
+        index_b = 0
+        best = generation[q_selected[0]]
+        for j in range(1, q):
+            if (generation[q_selected[j]][1] == best[1] and len(generation[q_selected[j]][0]) < len(best[0])) \
+                    or (generation[q_selected[j]][1] > best[1]):
+                fit_best = generation[q_selected[j]][1]
+                index_b = j
+                best = generation[q_selected[j]]
+
+        selected_chromosome.append(best[0])
+
+        if no_need_return_back_selected:
+            generation.pop(q_selected[index_b])
+
+    return selected_chromosome
+
+
+def get_final_result(dataset, chromosome, y_star, running_mode):
+    eval = evaluate_generation(dataset, chromosome, y_star, running_mode)
+    best_ch = eval[0][0]
+    best_fit = eval[0][1]
+    for i in range(len(eval)):
+        if eval[i][1] > best_fit:
+            best_fit = eval[i][1]
+            best_ch = eval[i][0]
+
+    chromosome = best_ch
+    # first calculate G matrix
+    dimension = dataset.shape[1]
+    g_matrix = []
+    for i in range(dataset.shape[0]):
+        data_i = dataset[i]
+        g_matrix.append(get_gi_of_a_data(data_i, chromosome, dimension))
+
+    # second calculate weights
+    w_matrix = calculate_weight_chromosome_i(g_matrix, y_star)
+
+    # third calculate Y matrix
+    y_matrix = calculate_y_out(g_matrix, w_matrix)
+
+    return [y_matrix, best_fit]
+
+
+def print_algorithm_parameters(dataset_length,
+                               initial_number_chromosomes,
+                               min_length_chromosome,
+                               max_length_chromosome,
+                               max_sigma_mutation):
+    print("Dataset size : " + str(dataset_length))
+    print("Initial generation size : " + str(initial_number_chromosomes))
+    print("Min Length of Chromosome : " + str(min_length_chromosome))
+    print("Max Length of Chromosome : " + str(max_length_chromosome))
+    print("Max Sigma in Mutation : " + str(max_sigma_mutation))
+    print("##############################")
--- run.py
+++ run.py
@@ -2,70 +2,135 @@
 import pandas as pd
 import argparse
 import functions
+from sklearn.preprocessing import MinMaxScaler
 
-parser = argparse.ArgumentParser(
-    description='This Programme try to train an RBF Network with the help of ES Algorithm')
 
-parser.add_argument('--input', help='Path of input data', default="Book1.xlsx")
-parser.add_argument('--cminlcr', help='Minimum Ratio of Chromosome Length in Regression', default="0.2")
-parser.add_argument('--cmaxlcr', help='Maximum Ratio of Chromosome Length', default="0.4")
-parser.add_argument('--init_ch_nu', help='Initial Ratio of Generation', default="0.4")
-parser.add_argument('--cmaxs', help='Maximum Ratio of Sigma', default="0.1")
-parser.add_argument('--reg_thr', help='threshold that indicate running mode', default="0.4")
+# running mode of Algorithm  = {Classification_2, Classification_n, Regression}
 
-args = parser.parse_args()
 
-# parameters
-min_length_chromosome = -1              # minimum number of basis in  each chromosome
-max_length_chromosome = -1              # maximum number of basis in  each chromosome
-max_sigma_mutation = -1                 # upper bound for sigma that using in mutation
-initial_number_chromosomes = -1         # number of initial generation
-dataset_length = 0                      # number of data in dataset
-data_dimension = 0
-algorithm_mode = 'Classification_2'
-# indicate running mode of Algorithm {Classification_2, Classification_n, Regression}
+def get_argument():
+    parser = argparse.ArgumentParser(
+        description='This Programme try to train an RBF Network with the help of ES Algorithm')
 
-args.cminlcr = float(args.cminlcr)
-args.cmaxlcr = float(args.cmaxlcr)
-args.init_ch_nu = float(args.init_ch_nu)
-args.cmaxs = float(args.cmaxs)
-args.reg_thr = float(args.reg_thr)
+    parser.add_argument('--input', help='Path of input data', default="simple_set1.xlsx")
+    parser.add_argument('--cminlcr', help='Minimum Ratio of Chromosome Length in Regression', default="0.2")
+    parser.add_argument('--cmaxlcr', help='Maximum Ratio of Chromosome Length', default="0.2")
+    parser.add_argument('--init_ch_nu', help='Initial Ratio of Generation', default="0.4")
+    parser.add_argument('--cmaxs', help='Maximum Ratio of Sigma', default="0.1")
+    parser.add_argument('--reg_thr', help='threshold that indicate running mode', default="0.4")
+    parser.add_argument('--q', help='threshold that indicate running mode', default="3")
 
-# reading data from file with excel format
-dataset_train = pd.read_excel(args.input)
-training_set = dataset_train.iloc[:, 0:dataset_train.shape[1]].values
-dataset_length = dataset_train.shape[0]
-data_dimension = dataset_train.shape[1] - 1
+    args = parser.parse_args()
+    args.cminlcr = float(args.cminlcr)
+    args.cmaxlcr = float(args.cmaxlcr)
+    args.init_ch_nu = float(args.init_ch_nu)
+    args.cmaxs = float(args.cmaxs)
+    args.reg_thr = float(args.reg_thr)
+    args.q = float(args.q)
 
-# initialization of parameters
-algorithm_mode, \
-min_length_chromosome,\
-max_length_chromosome,\
-max_sigma_mutation,\
-initial_number_chromosomes= functions.initialization_parameter(data_set=dataset_train,
-                                                               regression_threshold=args.reg_thr,
-                                                               ratio_min_length_chromosome_reg=args.cminlcr,
-                                                               ratio_max_length_chromosome=args.cmaxlcr,
-                                                               ratio_max_sigma=args.cmaxs,
-                                                               ratio_initial_number_chromosomes=args.init_ch_nu)
+    return args
 
-# producing initial generation
-generation = functions.create_initial_generation(data_set=dataset_train,
-                                                         min_length_chromosome=min_length_chromosome,
-                                                         max_length_chromosome=max_length_chromosome,
-                                                         generation_size=initial_number_chromosomes,
-                                                         max_sigma=max_sigma_mutation)
 
-iteration_steps = 1100
-for i in range(iteration_steps):
-    # selecting parents
-    parents = functions.selecting_parents_random_uniform(generation)
+def get_dataset(address):
+    dataset_train = pd.read_excel(address)
+    dataset_train_values = dataset_train.iloc[:, 0:dataset_train.shape[1] - 1].values
+    dataset_length = dataset_train_values.shape[0]
+    data_dimension = dataset_train_values.shape[1]
+    y_star = dataset_train.iloc[:, dataset_train.shape[1] - 1:dataset_train.shape[1]].values
+    y_star = y_star.reshape(len(y_star), 1)
 
-    # mutation
-    functions.do_mutation(parents, data_dimension)
+    return dataset_train, dataset_train_values, dataset_length, data_dimension, y_star
 
-    # recombination
-    childes = functions.recombination_chromosomes(parents)
 
-    # evaluate parents and childes
-    evaluated = functions.evaluate_generation(parents + childes, algorithm_mode)
+def evolutionary_strategy_core(dataset_train, dataset_train_values, dataset_length, data_dimension,
+                               y_star, normal_data, algorithm_mode, min_length_chromosome,
+                               max_length_chromosome, max_sigma_mutation, initial_number_chromosomes,
+                               iteration_steps, q, all_result):
+    # Print Information
+    functions.print_algorithm_parameters(dataset_length,
+                                         initial_number_chromosomes,
+                                         min_length_chromosome,
+                                         max_length_chromosome,
+                                         max_sigma_mutation)
+    # normalizing data
+    if normal_data:
+        sc = MinMaxScaler(feature_range=(0, 1))
+        dataset_train_values = sc.fit_transform(dataset_train_values)
+        dataset_train = sc.fit_transform(dataset_train)
+
+    # producing initial generation
+    generation = functions.create_initial_generation(data_set=dataset_train_values,
+                                                     min_length_chromosome=min_length_chromosome,
+                                                     max_length_chromosome=max_length_chromosome,
+                                                     generation_size=initial_number_chromosomes,
+                                                     max_sigma=max_sigma_mutation,
+                                                     data_set_raw=dataset_train)
+
+    for i in range(iteration_steps):
+        # selecting parents
+        parents = functions.selecting_parents_random_uniform(generation)
+
+        print("parent selected in " + str(i))
+        # mutation
+        functions.do_mutation(parents, data_dimension)
+
+        # recombination
+        childes = functions.recombination_chromosomes(parents)
+
+        # evaluate parents and childes
+        evaluated = functions.evaluate_generation(dataset_train_values, parents + childes, y_star, algorithm_mode)
+
+        # selection for next iteration
+        del generation
+        del childes
+        del parents
+        generation = functions.select_based_on_q_tournament(evaluated, q, initial_number_chromosomes)
+        print("iteration " + str(i) + " completed")
+
+    # select best chromosome
+    y_result = functions.get_final_result(dataset_train_values, generation, y_star, algorithm_mode)
+    all_result.append(y_result)
+
+
+def main():
+    # get parameters from user
+    args = get_argument()
+
+    # reading data from file with excel format
+    dataset_train, dataset_train_values, dataset_length, data_dimension, y_star = get_dataset(args.input)
+
+
+    # initialization of parameters
+    algorithm_mode, \
+    min_length_chromosome, \
+    max_length_chromosome, \
+    max_sigma_mutation, \
+    initial_number_chromosomes = functions.initialization_parameter(data_set=dataset_train,
+                                                                    regression_threshold=args.reg_thr,
+                                                                    ratio_min_length_chromosome_reg=args.cminlcr,
+                                                                    ratio_max_length_chromosome=args.cmaxlcr,
+                                                                    ratio_max_sigma=args.cmaxs,
+                                                                    ratio_initial_number_chromosomes=args.init_ch_nu)
+
+    all_result = []
+    normal_data = False
+    iteration_steps = 20
+
+    for i in range(5):
+        evolutionary_strategy_core(dataset_train, dataset_train_values, dataset_length, data_dimension,
+                                   y_star, normal_data, algorithm_mode, min_length_chromosome,
+                                   max_length_chromosome, max_sigma_mutation, initial_number_chromosomes,
+                                   iteration_steps, args.q, all_result)
+
+    best_res = all_result[0][0]
+    best_res_fit = all_result[0][1]
+    for i in range(1, len(all_result)):
+        if all_result[i][1] > best_res_fit:
+            best_res_fit = all_result[i][1]
+            best_res = all_result[i][0]
+    print(best_res)
+    print("fitness : " + str(best_res_fit))
+
+
+if __name__ == '__main__':
+    main()
